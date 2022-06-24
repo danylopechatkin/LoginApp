044989c40be13c11302fca773c38890b12fa7de7
Danylo <pechatkin.work@gmail.com>
2022-06-24 18:43:57 +0200

    last update logic

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -2,9 +2,7 @@
 //  LoginViewController.swift
 //  LoginApp
 //
-//  Created by Alexey Efimov on 19/06/2019.
-//  Copyright © 2019 Alexey Efimov. All rights reserved.
-//
+
 
 import UIKit
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -2,8 +2,7 @@
 //  WelcomeViewController.swift
 //  LoginApp
 //
-//  Created by Alexey Efimov on 19/06/2019.
-//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//  
 //
 
 import UIKit
